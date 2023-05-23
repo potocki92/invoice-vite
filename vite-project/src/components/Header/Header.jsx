a5697bfdbe5bb6406e5c8a16bc87ce6f4ab152c1
potocki92 <mateusz.potocki92@gmail.com>
2023-05-23 13:06:04 +0200

    burger icon

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -3,10 +3,19 @@ import { Link, useParams } from "react-router-dom";
 import "./Header.css";
 import { ReactComponent as LogoffIcon } from "../../assets/images/svg/logout.svg";
 import { homeLink } from "../../utils/linkConfig";
-import { HeaderStyled } from "./Header.styled";
+import {
+  BurgerIcon,
+  HeaderStyled,
+  MobileHeader,
+  ToggleMenuButton,
+} from "./Header.styled";
 
 const Header = ({ setLoginUser }) => {
-  let { id } = useParams();
+  const [isClick, setIsClick] = useState(false);
+  const handleBurgerClick = () => {
+    setIsClick(!isClick);
+  };
+
   const [user, setItems] = useState([]);
 
   const handleLogout = () => {
@@ -21,29 +30,44 @@ const Header = ({ setLoginUser }) => {
     }
   }, []);
   return (
-    <HeaderStyled>
-      <div className="container">
-        <div className="header__flex-row">
-          <div className="header__textContainer">
-            <span className="header__userName">{user?.user?.user?.name}</span>
-            <p className="header__userSpecification">User</p>
-          </div>
-          <Link to={`${homeLink}/user`}>
-            <button className="header-circle">
-              <svg className="header__svg" width={"24px"} height={"24px"}></svg>
+    <>
+      <MobileHeader>
+        <ToggleMenuButton>
+          <BurgerIcon className={isClick ? "active" : ""}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </BurgerIcon>
+        </ToggleMenuButton>
+      </MobileHeader>
+      <HeaderStyled>
+        <div className="container">
+          <div className="header__flex-row">
+            <div className="header__textContainer">
+              <span className="header__userName">{user?.user?.user?.name}</span>
+              <p className="header__userSpecification">User</p>
+            </div>
+            <Link to={`${homeLink}/user`}>
+              <button className="header-circle">
+                <svg
+                  className="header__svg"
+                  width={"24px"}
+                  height={"24px"}
+                ></svg>
+              </button>
+            </Link>
+            <button
+              className="header__button--logout"
+              onClick={() => handleLogout()}
+            >
+              <svg className="header__svg" width={"24px"} height={"24px"}>
+                <LogoffIcon />
+              </svg>
             </button>
-          </Link>
-          <button
-            className="header__button--logout"
-            onClick={() => handleLogout()}
-          >
-            <svg className="header__svg" width={"24px"} height={"24px"}>
-              <LogoffIcon />
-            </svg>
-          </button>
+          </div>
         </div>
-      </div>
-    </HeaderStyled>
+      </HeaderStyled>
+    </>
   );
 };
 
--- vite-project/src/components/Header/Header.styled.js
+++ vite-project/src/components/Header/Header.styled.js
@@ -16,6 +16,105 @@ export const HeaderStyled = styled.header`
   }
 `;
 
-export const BurgerButton = styled.button`
-  
-`
+export const MobileHeader = styled.div`
+  position: fixed;
+  right: 20px;
+  top: 13px;
+  z-index: 99;
+  display: none;
+
+  @media screen and (max-width: 1199px) {
+    display: block;
+  }
+`;
+export const ToggleMenuButton = styled.div`
+  width: 45px;
+  height: 45px;
+  position: relative;
+  border-radius: 50%;
+  cursor: pointer;
+  margin: 0;
+  padding: 0 15px;
+  border: none;
+  z-index: 100;
+  user-select: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
+  background-color: rgb(255, 255, 255);
+`;
+
+export const BurgerIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: transform 0.3s;
+  height: 100%;
+
+  span {
+    position: relative;
+    width: 150%;
+    margin-top: -4px;
+
+    &::before {
+      left: 0;
+      border-radius: 3px 0 0 3px;
+      content: "";
+      position: absolute;
+      top: 0;
+      background: currentColor;
+      height: 4px;
+      width: 50%;
+      transition: 0.25s cubic-bezier(0.6, 0, 0.3, 1);
+      transform-origin: center center;
+        }
+
+    &::after {
+      right: 0;
+      border-radius: 0 3px 3px 0;
+      content: "";
+      position: absolute;
+      top: 0;
+      background: currentColor;
+      height: 4px;
+      width: 50%;
+      transition: 0.25s cubic-bezier(0.6, 0, 0.3, 1);
+      transform-origin: center center;
+        }
+    }
+    span + span {
+      margin-top: 8px;
+    }
+  }
+
+  &.active {
+    span:nth-of-type(1):before {
+      transform: translate3d(3px, 4.5px, 0) rotate(45deg);
+    }
+
+    span:nth-of-type(1):after {
+      transform: translate3d(-3px, 4.5px, 0) rotate(-45deg);
+    }
+
+    span:nth-of-type(3):before {
+      transform: translate3d(3px, -5.5px, 0) rotate(-45deg);
+    }
+
+    span:nth-of-type(3):after {
+      transform: translate3d(-3px, -5.5px, 0) rotate(45deg);
+    }
+
+    span:nth-of-type(2):before, span:nth-of-type(2):after {
+      opacity: 0.0001;
+    }
+
+    span:nth-last-of-type(2)::before {
+      transform: translateX(-200%);
+    }
+
+    span:nth-last-of-type(2)::after {
+      transform: translateX(200%);
+    }
+    
+  }
+`;
