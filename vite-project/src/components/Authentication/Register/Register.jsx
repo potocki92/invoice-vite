f965780bc0495ee7bf83bec6493073f168c1db5e
potocki92 <40219157+potocki92@users.noreply.github.com>
2023-08-09 08:54:38 +0000

    forms/style

--- vite-project/src/components/Authentication/Register/Register.jsx
+++ vite-project/src/components/Authentication/Register/Register.jsx
@@ -62,6 +62,33 @@ const Register = ({ setShowRegister }) => {
     }
   };
 
+  const inputs = [
+    {
+      name: "name",
+      type: "text",
+      placeholder: "Name",
+      label: "Username",
+      required: true
+    },
+    {
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
+    },
+    {
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+  ]
   return (
     <LoginStyled>
       <LoginTitle>Register</LoginTitle>
--- vite-project/src/components/Common/FormsWrapper/FormsWrapper.styled.js
+++ vite-project/src/components/Common/FormsWrapper/FormsWrapper.styled.js
@@ -1,83 +1,57 @@
 import styled from "styled-components";
+
 export const FormsWrapperStyled = styled.div`
   background: linear-gradient(153.13deg, #c7edec 10.04%, #eefab3 91.46%),
     #e5fbba;
   min-height: 100vh;
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -webkit-flex-direction: column;
-  -ms-flex-direction: column;
   flex-direction: column;
-  -webkit-box-pack: justify;
-  -webkit-justify-content: space-between;
-  -ms-flex-pack: justify;
-  justify-content: space-between;
-  -webkit-box-align: center;
-  -webkit-align-items: center;
-  -ms-flex-align: center;
+  justify-content: center;
   align-items: center;
   margin-bottom: 15rem;
   @media (min-width: 1025px) {
-    -webkit-box-pack: center;
-    -webkit-justify-content: center;
-    -ms-flex-pack: center;
     justify-content: center;
-    -webkit-box-align: center;
-    -webkit-align-items: center;
-    -ms-flex-align: center;
     align-items: center;
   }
-  @media (min-width: 500px) {
-    -webkit-box-pack: center;
-    -webkit-justify-content: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-  }
 `;
+
 export const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100vh;
   padding: 2.5rem;
   background-color: #fff;
+  border-radius: 8px;
+  max-width: 34rem;
+  max-height: 40rem;
+  box-shadow: 6px 12px 60px rgba(0, 0, 0, 0.2);
+  width: 80vw; /* Używam 80vw dla płynności */
+  min-width: 200px;
   @media screen and (min-width: 1024px) {
-    margin-left: 10vw;
-    margin-right: 10vw;
-    border-radius: 8px;
-    max-width: 34rem;
-    max-height: 40rem;
-    -webkit-box-shadow: 6px 12px 60px rgba(0, 0, 0, 0.2);
-    box-shadow: 6px 12px 60px rgba(0, 0, 0, 0.2);
-  }
-  @media (min-width: 500px) {
-    width: 100%;
+    width: 480px;
+    margin-left: auto;
+    margin-right: auto;
   }
 `;
+
 export const Wrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 100%;
+  min-height: 500px;
   display: flex;
   justify-content: center;
   align-items: center;
-  @media (min-width: 1025px) {
-  }
 `;
+
 export const FormHeader = styled.div`
   margin-bottom: 5.5rem;
   position: absolute;
   top: 0;
   left: 0;
 `;
+
 export const FormTitle = styled.h1`
-  width: 7.3125rem;
   font-size: 2rem;
   line-height: 1;
 `;
