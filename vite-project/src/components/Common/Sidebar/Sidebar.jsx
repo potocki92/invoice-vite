5a59edb72d293b0e518d225ee60d1ff700fe2fef
potocki92 <40219157+potocki92@users.noreply.github.com>
2023-08-31 16:11:59 +0000

    fix

--- vite-project/src/components/Common/Sidebar/Sidebar.jsx
+++ vite-project/src/components/Common/Sidebar/Sidebar.jsx
@@ -14,6 +14,8 @@ import {
   SidebarTitle,
   ToggleMenuButton,
 } from "./Sidebar.styled";
+import { HomeOutline, PersonOutline } from 'react-ionicons'
+import styles from "./Sidebar.module.css"
 import { useState } from "react";
 import UserMenu from "@components/Common/UserMenu/UserMenu";
 
@@ -44,9 +46,14 @@ const Sidebar = ({ setLoginUser }) => {
   const handleLinkClick = () => {
     setIsClick(false);
   };
+  const [activeIndex, setActiveIndex] = useState(0); // State to keep track of the active index
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  }
   return (
-    <>
-      <MobileSidebar>
+    <div>
+      {/* <MobileSidebar>
         <ToggleMenuButton onClick={handleBurgerClick}>
           <BurgerIcon className={isClick ? "active" : ""}>
             <span></span>
@@ -115,8 +122,43 @@ const Sidebar = ({ setLoginUser }) => {
           setLoginUser={setLoginUser}
           handleLinkClick={handleLinkClick}
         />
-      </SidebarStyled>
-    </>
+      </SidebarStyled> */}
+  <div className={`${isClick ? "hidden" : ""} z-10 relative w-[300px] h-full bg-[#4b5bf9] border-l-[5px] border-[#4b5bf9]` } >
+      <ul className="absolute top-0 left-0 w-full pt-10 pl-[5px]">
+        <li 
+        className={`${activeIndex === 0 ? "bg-[#FBFCFF] text-red" : ""} relative w-full rounded-r-[20px] list`}
+        onClick={() => handleItemClick(0)}>
+          <b className="[&>*:nth-child(1)]:absolute"></b>
+          <b></b>
+          <a className="flex w-full relative block text-[#FBFCFF]" href="#">
+            <span className="relative block min-w-[60px] h-[60px] leading-[70px] text-center">
+              <HomeOutline
+                color={"currentColor"}
+                style={{ fontSize: "1.5em"}}
+              />
+            </span>
+            <span className={styles.title}>HOME</span>
+          </a>
+        </li>
+        <li 
+        className={`${activeIndex === 1 ? styles.active : ""} list`}
+        onClick={() => handleItemClick(1)}>
+          <b></b>
+          <b></b>
+          <a href="#">
+            <span className={styles.icon}>
+              <PersonOutline
+                color={"currentColor"}
+                style={{ fontSize: "1.5em"}}
+                />
+            </span>
+            <span className={styles.title}>PROFILE</span>
+          </a>
+        </li>
+      </ul>
+      </div>
+      <div className={styles.toggle} onClick={handleBurgerClick}></div>
+    </div>
   );
 };
 
