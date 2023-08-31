4998847cfa5e8c9fc6c5ef46e79e3cc7d583faab
potocki92 <40219157+potocki92@users.noreply.github.com>
2023-08-31 19:24:54 +0000

    c

--- vite-project/src/components/Common/Sidebar/Sidebar.jsx
+++ vite-project/src/components/Common/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { HomeOutline, PersonOutline } from 'react-ionicons'
 import { useState } from "react";
+import SidebarList from "./SidebarList/SidebarList";
 
 /**
  * Represents a sidebar navigation component that provides links to different sections of the application.
@@ -34,79 +35,24 @@ const Sidebar = ({ setLoginUser }) => {
   const handleItemClick = (index) => {
     setActiveIndex(index);
   }
+
+  const array = [
+    {
+      "title": "asdasd",
+    },
+    {
+      "title": "asdasd",
+    },
+    {
+      "title": "asdasd",
+    }
+  ]
   return (
     <div>
-      {/* <MobileSidebar>
-        <ToggleMenuButton onClick={handleBurgerClick}>
-          <BurgerIcon className={isClick ? "active" : ""}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </BurgerIcon>
-        </ToggleMenuButton>
-      </MobileSidebar>
-      <SidebarStyled className={`${isClick ? "menu-open" : ""}`}>
-        <SidebarTitle>InvCom</SidebarTitle>
-        <SidebarNavlink>
-          <SidebarList>
-            <Link to={""} onClick={handleLinkClick}>
-              <SidebarItem>
-                <SidebarButton>
-                  <svg className="sidebar__svg" width={"32px"} height={"32px"}>
-                    <DashboardIcon />
-                  </svg>
-                  <SidebarSpan>Dashboard</SidebarSpan>
-                </SidebarButton>
-              </SidebarItem>
-            </Link>
-            <Link to={`invoice`} onClick={handleLinkClick}>
-              <SidebarItem>
-                <SidebarButton>
-                  <svg className="sidebar__svg" width={"32px"} height={"32px"}>
-                    <AddInvoiceIcon />
-                  </svg>
-                  <SidebarSpan>Create Invoice</SidebarSpan>
-                </SidebarButton>
-              </SidebarItem>
-            </Link>
-            <Link to={`products`} onClick={handleLinkClick}>
-              <SidebarItem>
-                <SidebarButton>
-                  <svg className="sidebar__svg" width={"32px"} height={"32px"}>
-                    <DashboardIcon />
-                  </svg>
-                  <SidebarSpan>Products</SidebarSpan>
-                </SidebarButton>
-              </SidebarItem>
-            </Link>
-            <Link to={`clients`} onClick={handleLinkClick}>
-              <SidebarItem>
-                <SidebarButton>
-                  <svg className="sidebar__svg" width={"32px"} height={"32px"}>
-                    <ClientsIcon />
-                  </svg>
-                  <SidebarSpan>Clients</SidebarSpan>
-                </SidebarButton>
-              </SidebarItem>
-            </Link>
-            <Link to={`user`} onClick={handleLinkClick}>
-              <SidebarItem>
-                <SidebarButton>
-                  <svg className="sidebar__svg" width={"32px"} height={"32px"}>
-                    <ClientsIcon />
-                  </svg>
-                  <SidebarSpan>User</SidebarSpan>
-                </SidebarButton>
-              </SidebarItem>
-            </Link>
-          </SidebarList>
-        </SidebarNavlink>
-        <UserMenu
-          setLoginUser={setLoginUser}
-          handleLinkClick={handleLinkClick}
-        />
-      </SidebarStyled> */}
-  <div className={`${isClick ? "hidden" : ""} sm:block z-10 relative w-[100vw] sm:w-[300px] h-full bg-[#4b5bf9] border-l-[5px] border-[#4b5bf9]` } >
+      {array.map((a) => {
+        <SidebarList title={a.title}/>
+      })}
+  <div className={`${isClick ? "" : "hidden"} sm:block z-10 relative w-[100vw] sm:w-[300px] h-full bg-[#4b5bf9] border-l-[5px] border-[#4b5bf9]` } >
       <ul className="absolute top-0 left-0 w-full pt-10 pl-[5px] mt-[45px]">
         <li className={`${activeIndex === 0 ? "bg-[#FBFCFF]" : ""} relative w-full rounded-l-[20px] list`} onClick={() => handleItemClick(0)}>
           <b className={`${activeIndex === 0 ? "block" : "hidden"} absolute top-[-20px] h-[20px] w-full bg-[#FBFCFF] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#4b5bf9] before:rounded-br-[20px]`}></b>
@@ -131,12 +77,51 @@ const Sidebar = ({ setLoginUser }) => {
                 style={{ fontSize: "1.5em"}}
                 />
             </span>
-            <span className={"relative block pl-[10px] h-[60px] leading-[60px] whitespace-normal"}>Create Invoice</span>
+            <span className={"relative block pl-[10px] h-[60px] leading-[60px] whitespace-normal"}>CREATE INVOICE</span>
+          </Link>
+        </li>
+        <li className={`${activeIndex === 2 ? "bg-[#FBFCFF]" : ""} relative w-full rounded-l-[20px] list`} onClick={() => handleItemClick(2)}>
+          <b className={`${activeIndex === 2 ? "block" : "hidden"} absolute top-[-20px] h-[20px] w-full bg-[#FBFCFF] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#4b5bf9] before:rounded-br-[20px]`}></b>
+          <b className={`${activeIndex === 2 ? "block" : "hidden"} absolute bottom-[-20px] h-[20px] w-full bg-[#FBFCFF] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#4b5bf9] before:rounded-tr-[20px]`}></b>
+          <Link className={`${activeIndex === 2 ? "text-[#333]" : "text-[#FBFCFF]"} flex w-full relative block`} to={`products`} onClick={handleLinkClick}>
+            <span className="relative flex justify-center items-center min-w-[60px] h-[60px] leading-[70px] text-center">
+              <PersonOutline
+                color={"currentColor"}
+                style={{ fontSize: "1.5em"}}
+                />
+            </span>
+            <span className={"relative block pl-[10px] h-[60px] leading-[60px] whitespace-normal"}>PRODUCTS</span>
+          </Link>
+        </li>
+        <li className={`${activeIndex === 3 ? "bg-[#FBFCFF]" : ""} relative w-full rounded-l-[20px] list`} onClick={() => handleItemClick(3)}>
+          <b className={`${activeIndex === 3 ? "block" : "hidden"} absolute top-[-20px] h-[20px] w-full bg-[#FBFCFF] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#4b5bf9] before:rounded-br-[20px]`}></b>
+          <b className={`${activeIndex === 3 ? "block" : "hidden"} absolute bottom-[-20px] h-[20px] w-full bg-[#FBFCFF] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#4b5bf9] before:rounded-tr-[20px]`}></b>
+          <Link className={`${activeIndex === 3 ? "text-[#333]" : "text-[#FBFCFF]"} flex w-full relative block`} to={`clients`} onClick={handleLinkClick}>
+            <span className="relative flex justify-center items-center min-w-[60px] h-[60px] leading-[70px] text-center">
+              <PersonOutline
+                color={"currentColor"}
+                style={{ fontSize: "1.5em"}}
+                />
+            </span>
+            <span className={"relative block pl-[10px] h-[60px] leading-[60px] whitespace-normal"}>CLIENTS</span>
+          </Link>
+        </li>
+        <li className={`${activeIndex === 4 ? "bg-[#FBFCFF]" : ""} relative w-full rounded-l-[20px] list`} onClick={() => handleItemClick(4)}>
+          <b className={`${activeIndex === 4 ? "block" : "hidden"} absolute top-[-20px] h-[20px] w-full bg-[#FBFCFF] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#4b5bf9] before:rounded-br-[20px]`}></b>
+          <b className={`${activeIndex === 4 ? "block" : "hidden"} absolute bottom-[-20px] h-[20px] w-full bg-[#FBFCFF] before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[#4b5bf9] before:rounded-tr-[20px]`}></b>
+          <Link className={`${activeIndex === 4 ? "text-[#333]" : "text-[#FBFCFF]"} flex w-full relative block`} to={`user`} onClick={handleLinkClick}>
+            <span className="relative flex justify-center items-center min-w-[60px] h-[60px] leading-[70px] text-center">
+              <PersonOutline
+                color={"currentColor"}
+                style={{ fontSize: "1.5em"}}
+                />
+            </span>
+            <span className={"relative block pl-[10px] h-[60px] leading-[60px] whitespace-normal"}>USER</span>
           </Link>
         </li>
       </ul>
       </div>
-      <div className={`${isClick ? "bg-[#4b5bf9]" : "bg-[#FBFCFF]"} sm:hidden z-20 fixed top-[20px] right-[20px] w-[50px] h-[50px] rounded-[10px] cursor-pointer`} onClick={handleBurgerClick}></div>
+      <div className={`${isClick ? "bg-[#FBFCFF]" : "bg-[#4b5bf9]" } sm:hidden z-20 fixed top-[20px] right-[20px] w-[50px] h-[50px] rounded-[10px] cursor-pointer`} onClick={handleBurgerClick}></div>
     </div>
   );
 };
