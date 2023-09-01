204209fe4f624f5dc544ee25d3d069d5f6ffcb82
potocki92 <40219157+potocki92@users.noreply.github.com>
2023-09-01 19:33:36 +0000

    SidebarButton/update
    Center button and use full opacity to hover.

--- vite-project/src/components/Common/Sidebar/SidebarButton/SidebarButton.jsx
+++ vite-project/src/components/Common/Sidebar/SidebarButton/SidebarButton.jsx
@@ -5,11 +5,11 @@ import { MyContext } from "../Context";
   const {handleBurgerClick, isClick} = useContext(MyContext) 
 
     return (
-      <div className={`flex items-center sm:hidden p-[5px] z-20 fixed top-[20px] right-[25px] w-[50px] h-[40px] rounded-[10px] cursor-pointer`}
+      <div className={`flex items-center justify-center sm:hidden p-[5px] z-20 fixed top-[20px] right-[25px] w-[50px] h-[50px] rounded-[10px] cursor-pointer`}
         onClick={handleBurgerClick}>
-             <div className={`tham ${isClick ? "tham-active" : ""} tham-e-squeeze tham-w-8`}>
+             <div className={`tham ${isClick ? "tham-active" : ""} tham-e-squeeze tham-w-8 hover:opacity-100`}>
   <div class="tham-box">
-    <div className={`tham-inner ${isClick ? "bg-[#FBFCFF]" : "bg-[#4b5bf9]"} `} />
+    <div className={`tham-inner ${isClick ? "bg-[#FBFCFF]" : "bg-[#4b5bf9]"}`} />
   </div>
 </div>
       </div>
